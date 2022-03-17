dbcd622f63ee4f4e9e69d2a0cae561f14fdf2952
Kevin Lee <blackseeds10@hotmail.com>
2022-03-17 06:29:45 +0000

    fetcher

--- fetcher.js
+++ fetcher.js
@@ -1,13 +1,13 @@
-const args1 = process.argv.slice(2, 3);
+const args = process.argv.slice(2);
 const request = require('request');
-const fs = require('fs')
+const fs = require('fs');
 
 request(args[0], (error, response, body) => {
-  fs.writeFile(args[1]. body, error => {
+  fs.writeFile(args[1], body, (error) => {
     if (error) {
-      console.log(`Failed to write to localPaths: ${args[1]}`);
+      console.log("failed to write to localPaths", args[1])
     } else {
-      console.log(`Donwloaded and saved ${body.length} bytes to ${args[1]}`);
+      console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`)
     }
   })
 });
